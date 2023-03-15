e465f19e6d1a0a7accc1b3bf38122435df6052e9
Hoàng Anh <nth.anh020209@gmail.com>
2023-03-15 16:13:45 +0700

    fix css

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,16 @@
 function notAvailable() {
     alert('Tính năng này đang phát triển! vui lòng quay lại sau, xin cảm ơn!');
 }
+
+// function toggleCategory() {
+//     if (document.querySelector('.header__category-list-container').style.display == 'block') {
+//         document.querySelector('.header__category-list-container').style.display = 'none';
+//     } else {
+//         document.querySelector('.header__category-list-container').style.display = 'block';
+//     }
+// }
+
+// $(document).ready(function() {
+//     var $title = $('.header__category-title');
+//     var $category = $('.header__category-list-container');
+// })
