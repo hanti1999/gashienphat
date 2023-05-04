c1ef5b18dc3003680806cdfb76317cf8d9a3274b
Hoàng Anh <nth.anh020209@gmail.com>
2023-05-04 15:58:50 +0700

    add render code

---
+++ assets/script/renderGasStove.js
@@ -0,0 +1,277 @@
+var products = [
+    {
+        id: 1,
+        link: '#',
+        img: './assets/img/product/fukuda690.png',
+        name: 'Bếp gas đôi mặt kính Fukuda FK-690', 
+        warranty: './assets/img/2yearWarranty.png',
+        apr: 'Trả góp 0%',
+        sale: '51.000đ',
+        oldPrice: '1.250.000đ',
+        newPrice: '1.199.000đ',
+        gift: 'Tặng chảo chống dính Elmich 26cm',
+        description: [
+            'Mặt kính cường lực',
+            'Đầu đốt đồng thau',
+            'Điếu inox 304 chống gỉ sét',
+            'Kiềng gang siêu bền bỉ kèm chắn gió',
+            'Đánh lửa magneto (cơ)',
+            'Sản xuất tại Việt Nam'
+        ]
+    },
+    {
+        id: 2,
+        link: '#',
+        img: './assets/img/product/nasonal-na250d.png',
+        name: 'Bếp gas đơn mặt kính Nasonal NA 250', 
+        warranty: './assets/img/1yearWarranty.png',
+        apr: '',
+        sale: '20.000đ',
+        oldPrice: '360.000đ',
+        newPrice: '340.000đ',
+        gift: 'Tặng chảo chống dính Sunhouse 18cm',
+        description: [
+            'Mặt kính cường lực',
+            'Đầu đốt đồng thau',
+            'Kiềng thép phủ men',
+            'Đánh lửa magneto (cơ)',
+            'Sản xuất tại Việt Nam'
+        ]
+    },
+    {
+        id: 3,
+        link: '#',
+        img: './assets/img/product/lora.png',
+        name: 'Bếp gas đôi mặt kính LoraSun/Nasonal', 
+        warranty: './assets/img/2yearWarranty.png',
+        apr: '',
+        sale: '60.000đ',
+        oldPrice: '580.000đ',
+        newPrice: '520.000đ',
+        gift: 'Tặng chảo chống dính Elmich 26cm',
+        description: [
+            'Mặt kính cường lực',
+            'Đầu đốt đồng thau',
+            'Kiềng thép phủ men',
+            'Đánh lửa magneto (cơ)',
+            'Sản xuất tại Việt Nam'
+        ]
+    },
+    {
+        id: 4,
+        link: '#',
+        img: './assets/img/product/Akia.png',
+        name: 'Bếp gas đôi mặt kính Akia Slim', 
+        warranty: './assets/img/5yearWarranty.png',
+        apr: 'Trả góp 0%',
+        sale: '150.000đ',
+        oldPrice: '1.790.000đ',
+        newPrice: '1.640.000đ',
+        gift: 'Tặng chảo chống dính Elmich 26cm',
+        description: [
+            'Mặt kính cường lực',
+            'Đầu đốt đồng thau',
+            'Kiềng thép phủ men',
+            'Mâm hứng dầu rời kèm ron cao su cách nhiệt',
+            'Điếu inox 304 chống gỉ sét',
+            'Đánh lửa magneto (cơ)',
+            'Sản xuất tại Việt Nam'
+        ]
+    },
+    {
+        id: 5,
+        link: '#',
+        img: './assets/img/product/NamiluxPL2111AS.png',
+        name: 'Bếp gas mini NaMilux PL2111AS - Chống nổ Inline-Cut', 
+        warranty: './assets/img/1yearWarranty.png',
+        apr: '',
+        sale: '10.000đ',
+        oldPrice: '280.000đ',
+        newPrice: '270.000đ',
+        gift: '',
+        description: [
+            'Van an toàn Inline-Cut',
+            'Vỏ inox 304 dập không gỉ',
+            'Đánh lửa magneto (cơ)',
+            'Chân kiềng nhôm có chắn gió',
+            'Sản xuất tại Việt Nam'
+        ]
+    },
+    {
+        id: 6,
+        link: '#',
+        img: './assets/img/product/fukudaQD001.png',
+        name: 'Bếp gas đôi mặt kính Ogawa/Fukuda QD001', 
+        warranty: './assets/img/2yearWarranty.png',
+        apr: 'Trả góp 0%',
+        sale: '100.000đ',
+        oldPrice: '890.000đ',
+        newPrice: '790.000đ',
+        gift: 'Tặng chảo chống dính Elmich 26cm',
+        description: [
+            'Mặt kính cường lực',
+            'Đầu đốt đồng thau',
+            'Kiềng thép phủ men',
+            'Đánh lửa magneto (cơ)',
+            'Sản xuất tại Việt Nam'
+        ]
+    },
+    {
+        id: 7,
+        link: '#',
+        img: './assets/img/product/ThaiFlameCN.png',
+        name: 'Bếp gas cao áp khè ThaiFlame', 
+        warranty: '',
+        apr: '',
+        sale: '10.000đ',
+        oldPrice: '580.000đ',
+        newPrice: '570.000đ',
+        gift: '',
+        description: [
+            'Đầu đốt gang',
+            'Thân bếp gang',
+            'Đánh lửa magneto (cơ)',
+            'Sản xuất tại Việt Nam'
+        ]
+    },
+    {
+        id: 8,
+        link: '#',
+        img: './assets/img/product/Rinnai2i.png',
+        name: 'Bếp gas âm Rinnai RVB-2i(AB) - Tự động ngắt', 
+        warranty: './assets/img/5yearWarranty.png',
+        apr: 'Trả góp 0%',
+        sale: '200.000đ',
+        oldPrice: '5.300.000đ',
+        newPrice: '5.100.000đ',
+        gift: 'Tặng chảo chống dính Elmich 26cm',
+        description: [
+            'Mặt kính cường lực cao ấp',
+            'Đầu đốt bằng thép không gỉ công nghệ Inner Burner lửa xoắn',
+            'Kiềng thép phủ men kèm mâm hứng dầu rời chống gỉ',
+            'Đánh lửa IC dùng Pin',
+            'Cảm ứng tự động ngắt gas tự động khi gặp sự cố',
+            'Sản xuất tại Việt Nam'
+        ]
+    },
+    {
+        id: 9,
+        link: '#',
+        img: './assets/img/product/Sanka795.png',
+        name: 'Bếp gas âm Sanka SKT-795', 
+        warranty: './assets/img/2yearWarranty.png',
+        apr: 'Trả góp 0%',
+        sale: '300.000đ',
+        oldPrice: '2.490.000đ',
+        newPrice: '2.190.000đ',
+        gift: 'Tặng chảo chống dính Elmich 26cm',
+        description: [
+            'Mặt kính cường lực chịu nhiệt',
+            'Đánh lửa magneto (cơ)',
+            'Kiềng thép phủ men',
+            'Đầu đốt đồng thau',
+            'Điếu inox 304',
+            'Sản xuất tại Việt Nam',
+            'Kích thước cắt đá: 650x370mm'
+        ]
+    },
+    {
+        id: 10,
+        link: '#',
+        img: './assets/img/product/NamiluxPL1911PF.png',
+        name: 'Bếp gas mini Namilux PL1911PF Chống nổ Inline-cut', 
+        warranty: './assets/img/1yearWarranty.png',
+        apr: '',
+        sale: '10.000đ',
+        oldPrice: '260.000đ',
+        newPrice: '250.000đ',
+        gift: '',
+        description: [
+            'Van an toàn Inline-Cut',
+            'Vỏ tôn sơn đỏ',
+            'Đánh lửa magneto (cơ)',
+            'Chân kiềng nhôm có chắn gió',
+            'Sản xuất tại Việt Nam'
+        ]
+    },
+    {
+        id: 11,
+        link: '#',
+        img: './assets/img/product/Rinnai715SlimGL.png',
+        name: 'Bếp gas đôi mặt kính Rinnai 715 Slim(GL-D)', 
+        warranty: './assets/img/2yearWarranty.png',
+        apr: 'Trả góp 0%',
+        sale: '0đ',
+        oldPrice: '',
+        newPrice: '2.590.000đ',
+        gift: 'Tặng chảo chống dính Elmich 26cm',
+        description: [
+            'Mặt kính cường lực cao ấp',
+            'Đầu đốt đồng thau kèm đầu hâm',
+            'Kiềng thép phủ men',
+            'Đánh lửa magneto (cơ)',
+            'Điếu gang bền bỉ',
+            'Lượng gas tiêu thụ ~0.42kg/h',
+            'Sản xuất tại Việt Nam'
+        ]
+    },
+    {
+        id: 12,
+        link: '#',
+        img: './assets/img/product/Ogawa-640.png',
+        name: 'Bếp gas đôi Ogawa OG-640', 
+        warranty: './assets/img/2yearWarranty.png',
+        apr: 'Trả góp 0%',
+        sale: '51.000đ',
+        oldPrice: '1.250.000đ',
+        newPrice: '1.199.000đ',
+        gift: 'Tặng chảo chống dính Elmich 26cm',
+        description: [
+            'Mặt kính cường lực',
+            'Đầu đốt đồng thau',
+            'Điếu inox 304 chống gỉ sét',
+            'Kiềng gang siêu bền bỉ kèm chắn gió<',
+            'Đánh lửa magneto (cơ)',
+            'Sản xuất tại Việt Nam'
+        ]
+    }
+]
+
+function renderProduct(products) {
+    var htmls = products.map(function(product) {
+        return `
+            <!-- Product -->
+            <div class="col l-3 m-4 c-6">
+                <a href="${product.link}" class="product-item-link">
+                    <div class="product-item">
+                        <div class="product-item__head">
+                            <div class="product__img">
+                                <img src="${product.img}" alt="">
+                            </div>
+                            <div class="product__ribbon APR">${product.apr}</div>
+                            <div class="product__ribbon">Giảm ${product.sale}</div>
+                            <img src="${product.warranty}" class="product__warranty"></img>
+                        </div>
+                        <div class="product__name">
+                            <h1>${product.name}</h1>
+                        </div>
+                        <div class="product__price">
+                            <span class="product__price-old">${product.oldPrice}</span>
+                            <span class="product__price-current">${product.newPrice}</span>
+                        </div>
+                        <div class="product-info">
+                            <ul>
+                                ${product.description.map(function(des) {
+                                    return `<li>${des}</li>`
+                                }).join('')}
+                                <span>${product.gift}</span>
+                            </ul>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        `
+    })
+    document.querySelector('.gasStoveBlock').innerHTML = htmls.join('');
+};
+renderProduct(products);
---
+++ assets/script/renderHotSale.js
@@ -0,0 +1,122 @@
+var products = [
+    {
+        id: 1,
+        link: '#',
+        img: './assets/img/product/kaff-kf-fl866gih.png',
+        name: 'Bếp điện KAFF KF-FL866GII (1 từ 1 hồng ngoại)', 
+        warranty: './assets/img/5yearWarranty.png',
+        apr: 'Trả góp 0%',
+        sale: '4.000.000đ',
+        oldPrice: '14.800.000đ',
+        newPrice: '10.800.000đ',
+        gift: 'Lưu ý: đầu từ chỉ sử dụng với các sản phẩm có đáy từ',
+        description: [
+            'Mặt kính Ceramic cường lực, chịu lực, chịu sốc nhiệt',
+            'Đầu đốt EGO (Made in Germany)',
+            'Điều khiển cảm ứng trượt trên mặt bếp',
+            'Chức năng inverter và chia công suất tiết kiệm điện và bảo vệ bếp',
+            'Chức năng chống tràn, khóa trẻ em, cảm ứng quá nhiệt',
+            'Đa chức năng, hẹn giờ độc lập',
+            'Sản xuất tại Malaysia'
+        ]
+    },
+    {
+        id: 2,
+        link: '#',
+        img: './assets/img/product/Kangaroo-KG435i.png',
+        name: 'Bếp điện từ đôi Kangaroo KG435i', 
+        warranty: './assets/img/2yearWarranty.png',
+        apr: '',
+        sale: '2.200.000đ',
+        oldPrice: '8.000.000đ',
+        newPrice: '5.800.000đ',
+        gift: 'Lưu ý: bếp chỉ sử dụng với các sản phẩm có đáy từ',
+        description: [
+            'Mặt kính cường lực Ceramic chống bám nước, chống xước, chịu sốc nhiệt',
+            'Công nghệ dẫn nhiệt theo chiều dọc, tiết kiệm điện năng',
+            'Điều khiển cảm ứng trượt trên mặt bếp',
+            'Cảng báo khi không có nồi/sai loại nồi, khóa trẻ em, cảm ứng quá nhiệt',
+            'Đa chức năng, hẹn giờ độc lập',
+            'Thương hiệu Việt Nam'
+        ]
+    },
+    {
+        id: 3,
+        link: '#',
+        img: './assets/img/product/daikiosan-dsw-32009h2.png',
+        name: 'Máy lọc nước RO 9 lõi Daikiosan DSW 32009H2', 
+        warranty: './assets/img/2yearWarranty.png',
+        apr: '',
+        sale: '1.350.000đ',
+        oldPrice: '6.650.000đ',
+        newPrice: '5.300.000đ',
+        gift: 'Tặng chảo chống dính Elmich 26cm',
+        description: [
+            '9 cấp lọc',
+            'Linh kiệt nhập khẩu Mỹ, Đài Loan, ...',
+            'Chức năng: chỉ lọc',
+            'Công suất lọc: 10L/h',
+            'Bình áp: ~10L',
+            'Lắp ráp tại Việt Nam'
+        ]
+    },
+    {
+        id: 4,
+        link: '#',
+        img: './assets/img/product/mutosi-md-203.png',
+        name: 'Cây nước Nóng Lạnh Mutosi MD-203', 
+        warranty: './assets/img/2yearWarranty.png',
+        apr: 'Trả góp 0%',
+        sale: '140.000đ',
+        oldPrice: '4.090.000đ',
+        newPrice: '3.950.000đ',
+        gift: 'Lưu ý: Chỉ dùng bình nước đầu đứng',
+        description: [
+            'Công nghệ làm lạnh bằng Block giúp làm lạnh nhanh và sâu 6ºC ~ 12ºC',
+            'Làm nóng nhanh và cao 85ºC ~ 95ºC',
+            'Tủ chứa đồ lớn, lên tới 14L',
+            'Công tắc nóng lạnh riêng biệt',
+            'Vỏ tủ chắc chắn, thân tủ kín, bảo vệ các linh kiện bên trong',
+            'Thiết kế úp bình'
+        ]
+    }
+]
+
+function renderProduct(products) {
+    var htmls = products.map(function(product) {
+        return `
+            <!-- Product -->
+            <div class="col l-3 m-4 c-6">
+                <a href="${product.link}" class="product-item-link">
+                    <div class="product-item">
+                        <div class="product-item__head">
+                            <div class="product__img">
+                                <img src="${product.img}" alt="">
+                            </div>
+                            <div class="product__ribbon APR">${product.apr}</div>
+                            <div class="product__ribbon">Giảm ${product.sale}</div>
+                            <img src="${product.warranty}" class="product__warranty"></img>
+                        </div>
+                        <div class="product__name">
+                            <h1>${product.name}</h1>
+                        </div>
+                        <div class="product__price">
+                            <span class="product__price-old">${product.oldPrice}</span>
+                            <span class="product__price-current">${product.newPrice}</span>
+                        </div>
+                        <div class="product-info">
+                            <ul>
+                                ${product.description.map(function(des) {
+                                    return `<li>${des}</li>`
+                                }).join('')}
+                                <span>${product.gift}</span>
+                            </ul>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        `
+    })
+    document.querySelector('.hotSaleBlock').innerHTML = htmls.join('');
+};
+renderProduct(products);
